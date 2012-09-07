e65d783068458d1588c124be5a98ac6800444311
Samantha Cohen <cohensam@seas.upenn.edu>
2012-09-07 14:15:15 -0400

    New app just created!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secondapp::Application.config.secret_token = '1aa3f18b0a9b3d0a71357b84b7207f6d2ea4cb45cfd8df2028de19fd1b311e44508e89ef2c442b82f9f8bad44b57ba4060cf65702d88ebef56fe3ab13338a305'
